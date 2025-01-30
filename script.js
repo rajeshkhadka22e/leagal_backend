374591ed0a5d15b18ac14a88ea284fe284c890c4
Rajesh Khadka <rajeshkhadka22e@gmail.com>
2025-01-30 15:03:29 +0545

    adding contact form

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-export default function handler(req, res) {
-    try {
-      res.status(200).json({ message: "Function is working!" });
-    } catch (error) {
-      res.status(500).json({ error: "An unexpected error occurred." });
-    }
+
+
+  // leagel person
+  const slider = document.getElementById("slider");
+
+  function slideLeft() {
+      slider.scrollBy({ left: -310, behavior: "smooth" }); // Adjust scroll distance as needed
   }
-  
+
+  function slideRight() {
+      slider.scrollBy({ left: 310, behavior: "smooth" }); // Adjust scroll distance as needed
+  }
+
+
+
+  // faq section
+  function toggleFaq(id) {
+    let faqs = document.querySelectorAll("[id^=faq]");
+    
+    faqs.forEach(faq => {
+        if (faq.id !== "faq" + id) {
+            faq.classList.add("hidden");
+        }
+    });
+
+    document.getElementById("faq" + id).classList.toggle("hidden");
+}
+
+
+
+//  JavaScript for Toggle 
+
+    const menuToggle = document.getElementById("menu-toggle");
+    const mobileMenu = document.getElementById("mobile-menu");
+
+    menuToggle.addEventListener("click", () => {
+        mobileMenu.classList.toggle("hidden");
+    });
